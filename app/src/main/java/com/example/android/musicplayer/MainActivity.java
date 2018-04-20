9fa198d4f779bc555a1c15344126e9c2be9fd637
xkaraman <xenakiskaramanos@gmail.com>
2018-04-20 16:38:54 +0300

    Update Manifest Parent Activities

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.android.musicplayer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
